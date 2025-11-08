1ec3b17996febd93750728a783b84a5482ca2da7
KronosDP <darrel.danadyaksa19@gmail.com>
2025-11-08 19:20:25 +0700

    feat: integrate clarity_flutter and register additional plugins for connectivity and device info

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 import 'features/authentication/authentication.dart';
 import 'core/theme/app_theme.dart';
 import 'core/auth/ensure_guest_auth.dart';
+import 'package:clarity_flutter/clarity_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MainApp());
+  
+  final config = ClarityConfig(
+    projectId: "twlp5nx6fo",
+    logLevel: LogLevel.Verbose // Note: Use "LogLevel.Verbose" value while testing to debug initialization issues.
+  );
+
+  runApp(ClarityWidget(
+    app: MainApp(),
+    clarityConfig: config,
+  ));
 }
 
 class MainApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,21 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import device_info_plus
 import file_picker
 import flutter_secure_storage_macos
+import package_info_plus
 import path_provider_foundation
 import record_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
