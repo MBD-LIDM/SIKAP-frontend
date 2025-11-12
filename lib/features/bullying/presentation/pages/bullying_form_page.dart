3c811b1ab04b1ef31a06873eb0231ae61788ff7b
Adjie <adjiesidja20@gmail.com>
2025-11-12 11:25:25 +0700

    save dulu aja

--- lib/features/bullying/presentation/pages/bullying_form_page.dart
+++ lib/features/bullying/presentation/pages/bullying_form_page.dart
@@ -47,7 +47,7 @@ class _BullyingFormPageState extends State<BullyingFormPage> {
     _auth = AuthHeaderProvider(
       loadUserToken: () async => null, // guest-only flow
       loadGuestToken: () async => await _session.loadGuestToken(),
-      loadGuestId: () async => await _session.loadGuestId(),
+      loadGuestId: () async => await _session.loadGuestId()
     );
     _repo = BullyingRepository(
       apiClient: _api,
